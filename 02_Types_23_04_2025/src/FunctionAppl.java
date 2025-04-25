bd06c39583792a62f37c77674e79bbe7ab69c9eb
Pumpkin <piratdobra@gmail.com>
2025-04-26 00:00:16 +0300

    HomeworkStart

---
+++ 02_Types_23_04_2025/src/FunctionAppl.java
@@ -0,0 +1,16 @@
+public class FunctionAppl {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        iamprogrammer();
+    }
+
+
+    public static void iamprogrammer(){
+        System.out.println("I am programmer!!!");
+        author();
+    }
+
+    private static void author() {
+        System.out.println("John Smith");
+    }
+}
---
+++ 02_Types_23_04_2025/src/TypesAppl.java
@@ -0,0 +1,25 @@
+public class TypesAppl {
+    public static void main(String[] args){
+        byte bits = -128;
+        System.out.println("byte: " + bits);
+        short a = 32_767;
+        System.out.println("short: " + a);
+        int b = -2_147_483_648;
+        System.out.println("int: " + b);
+        long l = -2_147_483_649L;
+        System.out.println("long: " + l);
+        float f = 20.9f;
+        System.out.println("float: " + f);
+        double d = 4.2;
+        System.out.println("double: " + d);
+        char c =37;
+        System.out.println("char: " + c);
+        boolean flag = true;
+        System.out.println("boolean: " + flag);
+
+
+
+
+
+    }
+}

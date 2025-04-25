0759917e70366567857d180701bedea6153fe2db
Pumpkin <piratdobra@gmail.com>
2025-04-26 00:38:38 +0300

    Homework

--- 02_Types_23_04_2025/src/Homework.java
+++ 02_Types_23_04_2025/src/Homework.java
@@ -1,16 +1,54 @@
-public class FunctionAppl {
+public class Homework {
+    private static byte i=0;
+
     public static void main(String[] args){
-        System.out.println("Hello world!");
-        iamprogrammer();
+
+        System.out.println("Crazy?");
+
+        FuncSix();
+
+        FuncOne();
+        FuncTwo();
     }
 
 
-    public static void iamprogrammer(){
-        System.out.println("I am programmer!!!");
-        author();
+    public static void FuncOne(){
+        System.out.println("They put me in a room");
+        FuncFour();
     }
+    public static void FuncTwo(){
+        System.out.println("Crazy?");
+        FuncSix();
+    }
+    public static void FuncThree(){
+        System.out.println("Why am I here? Is this some sort of a joke? Or is it some kind of a political comment on a cruel fate of voiceless groups who are still forced to participate in order to be demonstrably ignored?  ");
 
-    private static void author() {
-        System.out.println("John Smith");
+    }
+    public static void FuncFour(){
+        System.out.println("A rubber room");
+        FuncFive();
+    }
+    public static void FuncFive(){
+        System.out.println("A rubber room with rats");
+
+        FuncSix();
+    }
+    public static void FuncSix(){
+
+
+        if (i==0) {
+            System.out.println("I was crazy once");
+            i++;
+        }
+        else  {
+            System.out.println("The rats made me crazy");
+            i=0;
+        }
+
+
+
+
     }
 }
+
+
